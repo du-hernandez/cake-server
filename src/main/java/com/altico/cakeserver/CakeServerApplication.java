55899d4195d885986fec69baffe35d73dcd341a2
Duverney Hernandez Mora <du.hernandez@udla.edu.co>
2025-05-27 16:06:00 -0500

    Initial scaffolding project

---
+++ src/main/java/com/altico/cakeserver/CakeServerApplication.java
@@ -0,0 +1,13 @@
+package com.altico.cakeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CakeServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CakeServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/altico/cakeserver/CakeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.altico.cakeserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CakeServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
