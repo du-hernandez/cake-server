bde72db2e9b962f6e66e922f1f007bb411de23a2
Duverney Hernandez Mora <du.hernandez@udla.edu.co>
2025-05-28 23:52:09 -0500

    Adjusts the component scan

--- src/main/java/com/altico/cakeserver/CakeServerApplication.java
+++ src/main/java/com/altico/cakeserver/CakeServerApplication.java
@@ -2,8 +2,12 @@ package com.altico.cakeserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.altico.cakeserver.infrastructure.adapters.output.persistence.repository")
+@EntityScan(basePackages = "com.altico.cakeserver.infrastructure.adapters.output.persistence.entity")
 public class CakeServerApplication {
 
     public static void main(String[] args) {
