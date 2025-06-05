ccb445c07528fede9b858181cf2ec5c15ee61afa
Duverney Hernandez Mora <du.hernandez@udla.edu.co>
2025-06-05 01:26:31 -0500

    Establece JwtProperties como una clase inmutable (no permite setters)

--- src/main/java/com/altico/cakeserver/infrastructure/config/JwtProperties.java
+++ src/main/java/com/altico/cakeserver/infrastructure/config/JwtProperties.java
@@ -7,12 +7,12 @@ import org.springframework.stereotype.Component;
 
 @Component
 @Getter
-@Setter
+//@Setter
 @ConfigurationProperties(prefix = "app.jwt")
 public class JwtProperties {
 
     private String secret;
-    private long expirationMs = 86400000;
-    private long refreshExpirationMs = 604800000;
+    private final long expirationMs = 86400000;
+    private final long refreshExpirationMs = 604800000;
     private String issuer;
 }
