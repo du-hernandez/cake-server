cfa2fb934c715b0c22013317b5df7a790504b8d4
Duverney Hernandez Mora <du.hernandez@udla.edu.co>
2025-06-05 03:48:19 -0500

    Se elimina dependencia no utilizada

--- src/main/java/com/altico/cakeserver/infrastructure/adapters/input/rest/AuthController.java
+++ src/main/java/com/altico/cakeserver/infrastructure/adapters/input/rest/AuthController.java
@@ -21,7 +21,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Set;
 import java.util.stream.Collectors;
 
 @RestController
